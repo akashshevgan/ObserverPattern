6064d7121dfd06019f2de30fb76399246e9a11e3
akash <akash@MSI>
2021-06-06 14:10:19 +0530

    Workshop 3 FileIO

---
+++ src/main/java/MumbaiAajTak.java
@@ -0,0 +1,13 @@
+public class MumbaiAajTak implements IObserver {
+
+
+    @Override
+    public void onUpdate(News news) {
+        System.out.println(news.getNews());
+    }
+
+    public NewsType getNewsType() {
+        return NewsType.MUMBAI_NEWS;
+    }
+}
+
